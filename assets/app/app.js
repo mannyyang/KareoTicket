5c2d7137f29e45789b419aa6a69a02e7a0f0ff32
Manuel Yang <me@manuelyang.com>
2014-12-17 08:53:43 -0800

    initial build with angular material.

---
+++ assets/app/app.js
@@ -0,0 +1,10 @@
+(function(angular){
+    'use strict';
+
+    angular.module('WebRequestsApp', [
+        'ngMaterial'
+    ])
+    .controller('AppCtrl', function($scope) {
+    });;
+
+})(window.angular);
