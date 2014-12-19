234a15d6c33cc38010bf823e11b8b646f17ca8a1
Manuel Yang <manuelyang@me.com>
2014-12-18 23:10:11 -0800

    updated angular controller.

--- assets/app/app.js
+++ assets/app/app.js
@@ -4,7 +4,15 @@
     angular.module('WebRequestsApp', [
         'ngMaterial'
     ])
-    .controller('ProjectsCtrl', function($scope) {
-    });;
+    .controller('ProjectsCtrl', function($scope, $http) {
+        // Simple GET request example :
+        $http.get('/api/getallprojects').
+            success(function(data, status, headers, config) {
+                console.log('success ' + data);
+            }).
+            error(function(data, status, headers, config) {
+                console.log('error ' + data);
+            });
+    });
 
 })(window.angular);
