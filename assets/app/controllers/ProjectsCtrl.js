3d92e7200b6300b53ab64fda8ef2ebf2388540be
Manuel Yang <manuelyang@me.com>
2014-12-20 00:33:40 -0800

    add projects controller for fronted.

---
+++ assets/app/controllers/ProjectsCtrl.js
@@ -0,0 +1,23 @@
+(function(){
+    "use strict";
+
+    var app = angular.module('WebRequestsApp');
+    app.controller('ProjectsCtrl', ProjectsCtrl);
+
+    function ProjectsCtrl($http){
+
+        var parentThis = this;
+
+        // GET all projects in database
+        $http.get('/api/getallprojects').
+            success(function (data, status, headers, config) {
+                console.log('success ' + data);
+                parentThis.projects = data;
+            }).
+            error(function (data, status, headers, config) {
+                console.log('error ' + data);
+            });
+    }
+
+
+})();
